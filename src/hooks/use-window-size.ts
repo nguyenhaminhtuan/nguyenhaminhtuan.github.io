ff524808a55733bb821477a012c0e3700e043021
Nguyen Ha Minh Tuan <nguyenhaminhtuan1997@gmail.com>
2021-07-22 17:36:14 +0700

    fix: window is not defined when prerender

--- src/hooks/use-window-size.ts
+++ src/hooks/use-window-size.ts
@@ -2,23 +2,25 @@ import { useEffect, useState } from 'react';
 
 function useWindowSize() {
   const [size, setSize] = useState<{ width: number; height: number }>({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: process.browser ? window.innerWidth : 0,
+    height: process.browser ? window.innerHeight : 0,
   });
 
   useEffect(() => {
-    const handler = () => {
-      setSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
+    if (process.browser) {
+      const handler = () => {
+        setSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
 
-    window.addEventListener('resize', handler);
+      window.addEventListener('resize', handler);
 
-    return () => {
-      window.removeEventListener('resize', handler);
-    };
+      return () => {
+        window.removeEventListener('resize', handler);
+      };
+    }
   }, []);
 
   return size;
