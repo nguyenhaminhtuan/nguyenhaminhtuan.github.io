7e39b786ae024494fe7a35b066594fef49b68fae
Nguyen Ha Minh Tuan <nguyenhaminhtuan1997@gmail.com>
2021-07-22 22:07:41 +0700

    fix: remove wrong prefix path

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,58 +1,57 @@
 import { css } from '@emotion/react';
-import prefix from '../utils/prefix';
 
 const typography = css`
   @font-face {
     font-family: 'Hackernoon';
-    src: url('${prefix}/fonts/HackerNoonV1-Regular.ttf');
+    src: url('/fonts/HackerNoonV1-Regular.ttf');
   }
   @font-face {
     font-family: 'IBM Plex Mono';
-    src: url('${prefix}/fonts/IBMPlexMono-Regular.ttf');
+    src: url('/fonts/IBMPlexMono-Regular.ttf');
   }
   @font-face {
     font-family: 'IBM Plex Mono';
     font-weight: 500;
-    src: url('${prefix}/fonts/IBMPlexMono-Medium.ttf');
+    src: url('/fonts/IBMPlexMono-Medium.ttf');
   }
   @font-face {
     font-family: 'IBM Plex Mono';
     font-weight: 600;
-    src: url('${prefix}/fonts/IBMPlexMono-SemiBold.ttf');
+    src: url('/fonts/IBMPlexMono-SemiBold.ttf');
   }
   @font-face {
     font-family: 'IBM Plex Mono';
     font-weight: bold;
-    src: url('${prefix}/fonts/IBMPlexMono-Bold.ttf');
+    src: url('/fonts/IBMPlexMono-Bold.ttf');
   }
   @font-face {
     font-family: 'IBM Plex Mono';
     font-style: italic;
-    src: url('${prefix}/fonts/IBMPlexMono-Italic.ttf');
+    src: url('/fonts/IBMPlexMono-Italic.ttf');
   }
   @font-face {
     font-family: 'IBM Plex Sans';
-    src: url('${prefix}/fonts/IBMPlexSans-Regular.ttf');
+    src: url('/fonts/IBMPlexSans-Regular.ttf');
   }
   @font-face {
     font-family: 'IBM Plex Sans';
     font-weight: 500;
-    src: url('${prefix}/fonts/IBMPlexSans-Medium.ttf');
+    src: url('/fonts/IBMPlexSans-Medium.ttf');
   }
   @font-face {
     font-family: 'IBM Plex Sans';
     font-weight: 600;
-    src: url('${prefix}/fonts/IBMPlexSans-SemiBold.ttf');
+    src: url('/fonts/IBMPlexSans-SemiBold.ttf');
   }
   @font-face {
     font-family: 'IBM Plex Sans';
     font-weight: bold;
-    src: url('${prefix}/fonts/IBMPlexSans-Bold.ttf');
+    src: url('/fonts/IBMPlexSans-Bold.ttf');
   }
   @font-face {
     font-family: 'IBM Plex Sans';
     font-style: italic;
-    src: url('${prefix}/fonts/IBMPlexSans-Italic.ttf');
+    src: url('/fonts/IBMPlexSans-Italic.ttf');
   }
 `;
 
