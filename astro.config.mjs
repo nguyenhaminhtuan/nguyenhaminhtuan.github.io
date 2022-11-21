a03f8e9bd3c92f0f3bd880e4d6c80b6d519b3fe8
Nguyen Ha Minh Tuan <nguyenhaminhtuan1997@gmail.com>
2022-11-21 22:10:24 +0700

    add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nguyenhaminhtuan.github.io',
   integrations: [tailwind(), react()],
 })
